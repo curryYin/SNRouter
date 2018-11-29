f971858acf2261abad8e7e659c79c2f7834ab592
aSnailNEO <asnailneo@gmail.com>
2018-11-29 18:03:43 +0800

    Update SNBaseViewController.m
    update msg

--- SNRouterDemo/SNRouterDemo/SNBaseViewController.m
+++ SNRouterDemo/SNRouterDemo/SNBaseViewController.m
@@ -24,7 +24,7 @@
     [super viewDidLoad];
     self.automaticallyAdjustsScrollViewInsets = NO;
     self.view.backgroundColor = [UIColor whiteColor];
-    
+    //commit
     UILabel *remind = [[UILabel alloc] initWithFrame:CGRectMake(10, 30, self.view.frame.size.width - 20, 0)];
     remind.text = @"轻触屏幕返回";
     [remind sizeToFit];
